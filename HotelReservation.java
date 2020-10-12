34fdf3475b343e093cd22f00a9537fbd9483a628
Suyash Jain <jain.suyash28@gmail.com>
2020-10-12 23:45:18 +0530

    Added: Master Welcome Added to the Reservation System

---
+++ HotelReservation.java
@@ -0,0 +1,8 @@
+package com.capgemini.hotelreservation;
+
+public class HotelReservation {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation System");
+	}
+}
+
