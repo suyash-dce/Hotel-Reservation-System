fb6316b8ad097f29b2da44fa019e2c36768c503b
suyash-dce <jain.suyash28@gmail.com>
2020-10-13 00:07:30 +0530

    Modified: 3 Hotels Added to Main Function in HRS

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,8 +1,12 @@
 package com.capgemini.hotelreservation;
 
 public class HotelReservation {
+
+	private final static Hotel LAKEWOOD = new Hotel("Lakewood", 110);
+	private final static Hotel BRIDGEWOOD = new Hotel("BridgeWood", 150);
+	private final static Hotel RIDGEWOOD = new Hotel("Ridgewood", 220);
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation System");
+		System.out.println("Welcome to Hotel Reservation Program");
 	}
 }
-
